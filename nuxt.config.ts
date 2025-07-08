509bda7c0c7829b60e74efe5654ee1588f1d4437
MrA1910 <153997016+MrA1910@users.noreply.github.com>
2025-07-08 17:18:57 +0700

    Setup Vuetify and Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // Add Vuetify styles
+  css: [
+    'vuetify/styles'
+  ],
+
+  // Enable Pinia
+  modules: [
+    '@pinia/nuxt'
+  ]
 })
